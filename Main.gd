fc5b750a709e5c2c62a70fa19f80085d4aa34683
K1aymore <klaymorer@protonmail.com>
2022-05-19 08:54:11 -0700

    Can click background to deselect all, movement ends smoother

--- Main.gd
+++ Main.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+var team = "team1"
 
 func _process(delta):
 	if Input.is_action_just_pressed("command"):
 		 get_tree().call_group("selected", "move")
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("select"):
+		for i in get_tree().get_nodes_in_group("selected"):
+			i.remove_from_group("selected")
--- Unit.gd
+++ Unit.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 const SPEED = 300
+const MOVEBUFFER = 3
 
 var mouseIn = false
 onready var movePos = position
@@ -8,34 +9,29 @@ onready var movePos = position
 var targets = []
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Circle.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if position != movePos:
+	if position.distance_to(movePos) > MOVEBUFFER:
 		move_and_slide(Vector2(movePos.x-position.x, movePos.y-position.y).normalized() * SPEED)
+	else:
+		position = movePos
 	
+	if is_in_group("selected"):
+		$Circle.visible = true
+	else:
+		$Circle.visible = false
 
 
-func _on_Unit_mouse_entered():
-	mouseIn = true
-	print("hello")
-func _on_Unit_mouse_exited():
-	mouseIn = false
-	print("goodbye")
-
-
-func _input(event):
-	if Input.is_action_just_pressed("select") and mouseIn:
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("select") and mouseIn and is_in_group("team1"):
+		get_tree().set_input_as_handled()
 		if is_in_group("selected"):
 			remove_from_group("selected")
-			$Circle.visible = false
 		else:
 			add_to_group("selected")
-			$Circle.visible = true
 
 
 func move():
@@ -44,6 +40,10 @@ func move():
 
 
 
+func _on_Unit_mouse_entered():
+	mouseIn = true
+func _on_Unit_mouse_exited():
+	mouseIn = false
 
 func _on_Range_body_entered(body):
 	if is_in_group("team1") && body.is_in_group("team2") \
